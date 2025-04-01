e939f2949e0878a6cfe0288e10153dba1070b24e
Aashu Khatkar <Aashukhatkar2006@gmail.com>
2025-04-01 09:24:43 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
-void rotateRight(int arr[], int n, int k) {
-    k %= n;
-   for (int i = 0; i < k; i++) {
-        int temp = arr[n-1];
-        for (int j = n-1; j > 0; j--) {
-            arr[j] = arr[j-1];
-        }
-        arr[0] = temp;
+
+// Function to reverse a portion of the array
+void reverse(int arr[], int start, int end) {
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
     }
 }
+
+// Function to rotate the array to the right by k positions
+void rotateArray(int arr[], int n, int k) {
+    k = k % n; // In case k is greater than n
+    reverse(arr, 0, n - 1);
+    reverse(arr, 0, k - 1);
+    reverse(arr, k, n - 1);
+}
+
+// Function to print the array
 void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) printf("%d", arr[i]);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
     printf("\n");
 }
+
 int main() {
     int n, k;
     scanf("%d", &n);
@@ -21,10 +35,10 @@ int main() {
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
+
     scanf("%d", &k);
 
-    printArray(arr, n);
-    rotateRight(arr, n, k);
+    rotateArray(arr, n, k);
     printArray(arr, n);
 
     return 0;
